0320b450013fc3fff587f0dc5edda5b32d9228a6
Jefferson Tarira <jatt6076@gmail.com>
2019-02-06 16:20:15 -0500

    update new select metricas dao

--- src/java/metricas/MetricasDAO.java
+++ src/java/metricas/MetricasDAO.java
@@ -21,11 +21,28 @@ public class MetricasDAO {
           + "group by 1, 2\n"
           + "order by 1";
 
+  private final String otherSelect = "select date(i.fecha) as fecha,\n"
+          + "       o.descripcion,\n"
+          + "       count(i.id)   as conteo\n"
+          + "from incidentes i,\n"
+          + "     origen o\n"
+          + "where i.id_origen = o.id\n"
+          + "  and (date(i.fecha) between '2017-03-01' and '2017-03-31')\n"
+          + "group by 1, 2\n"
+          + "order by 1;\n"
+          + "\n"
+          + "select o.descripcion, extract(year from i.fecha), count(*)\n"
+          + "from incidentes i,\n"
+          + "     origen o\n"
+          + "where i.id_origen = o.id\n"
+          + "group by 1,2\n"
+          + "order by 1,2";
+
   public MetricasDAO() {
     con = new ConexionRedshift();
   }
 
-  public ArrayList<Incidentes> getConteoPorRangoFechas() 
+  public ArrayList<Incidentes> getConteoPorRangoFechas()
           throws ClassNotFoundException, SQLException {
     ArrayList<Incidentes> lista = new ArrayList<>();
 
